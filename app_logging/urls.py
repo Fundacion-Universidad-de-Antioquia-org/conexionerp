c4218b43e0624fb4f6437ad2f1f01a5b4797b412
AngieCard <83438467+AngieCard@users.noreply.github.com>
2024-07-31 13:41:44 -0500

    creacion modelo logs

--- app_logging/urls.py
+++ app_logging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from app_logging.views import registrar_log
 
 urlpatterns = [
+    path('registrar/', registrar_log, name='registrar_log'),
     # Definir las rutas aquí, por ejemplo:
     # path('upload/', views.upload_file, name='upload_file'),
 ]
--- app_logging/views.py
+++ app_logging/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import Log
+import json
 
-# Create your views here.
+@csrf_exempt
+def registrar_log(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        correo = data.get('correo')
+        fecha = data.get('fecha')
+        tipo_evento = data.get('tipo_evento')
+        observacion = data.get('observacion') 
+        nombre_aplicacion = data.get('nombre_aplicacion') 
+        tipo = data.get('tipo') 
+
+        log = Log.objects.create(correo=correo, fecha=fecha, tipo_evento=tipo_evento,observacion=observacion, nombre_aplicacion=nombre_aplicacion,tipo=tipo)
+        return JsonResponse({'message': 'Log registrado correctamente'}, status=201)
+
+    return JsonResponse({'error': 'Método no permitido'}, status=405)
