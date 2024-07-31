71dd3191d380bbdb5427faafe667ea4fa975e432
AngieCard <83438467+AngieCard@users.noreply.github.com>
2024-07-31 15:33:24 -0500

    url

--- app_logging/urls.py
+++ app_logging/urls.py
@@ -3,7 +3,7 @@ from app_logging.views import registrar_log,consultar_logs
 
 urlpatterns = [
     path('registrar/', registrar_log, name='registrar_log'),
-    path('consultar-logs/', consultar_logs, name='consultar_logs'),
+    path('consultar/', consultar_logs, name='consultar_logs'),
 
     # Definir las rutas aquí, por ejemplo:
     # path('upload/', views.upload_file, name='upload_file'),
