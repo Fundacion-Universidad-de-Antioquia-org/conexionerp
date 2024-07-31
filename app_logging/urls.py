63d804daa73516d80ad943aa402e49d3e96d8880
AngieCard <83438467+AngieCard@users.noreply.github.com>
2024-07-31 15:30:58 -0500

    vista de logs

--- app_logging/urls.py
+++ app_logging/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from app_logging.views import registrar_log
+from app_logging.views import registrar_log,consultar_logs
 
 urlpatterns = [
     path('registrar/', registrar_log, name='registrar_log'),
+    path('consultar-logs/', consultar_logs, name='consultar_logs'),
+
     # Definir las rutas aquí, por ejemplo:
     # path('upload/', views.upload_file, name='upload_file'),
 ]
--- app_logging/views.py
+++ app_logging/views.py
@@ -1,7 +1,12 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import Log
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.dateparse import parse_date
+import datetime
 import json
+
 
 @csrf_exempt
 def registrar_log(request):
@@ -18,3 +23,23 @@ def registrar_log(request):
         return JsonResponse({'message': 'Log registrado correctamente'}, status=201)
 
     return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+
+@csrf_exempt
+def consultar_logs(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        correo = data.get('correo')
+        fecha = parse_date(data.get('fecha'))
+        if correo and fecha:
+            # Obtener la fecha del día anterior
+            fecha_anterior = fecha - datetime.timedelta(days=1)
+            # Verificar si existe un registro con la fecha del día anterior y estado 'SUCCESS'
+            registro_existe = Log.objects.filter(correo=correo, fecha__date=fecha_anterior, tipo_evento='SUCCESS').exists()
+            
+            if registro_existe:
+                return JsonResponse({'status': 'existe_registro', 'fecha_actual': str(fecha)})
+            else:
+                return JsonResponse({'status': 'no_existe_registro', 'fecha_anterior': str(fecha_anterior)})
+        return JsonResponse({'status': 'error', 'message': 'Correo o fecha no proporcionados'}, status=400)
+    return JsonResponse({'status': 'error', 'message': 'Método no permitido'}, status=405)
