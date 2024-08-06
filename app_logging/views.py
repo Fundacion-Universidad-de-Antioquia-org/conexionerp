698880b9ed7f341f4b6afcc7067111d554ae7328
AngieCard <83438467+AngieCard@users.noreply.github.com>
2024-08-06 10:53:01 -0500

    Cambios reporte medio dia

--- app_logging/views.py
+++ app_logging/views.py
@@ -1,6 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
 import json
-from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.http import JsonResponse
 from .models import Log
@@ -25,7 +24,7 @@ def registrar_log(request):
     return JsonResponse({'error': 'Método no permitido'}, status=405)
 
 logger = logging.getLogger(__name__)
-
+"""
 def update_log_date(request):
     logger.debug("Request received for update_log_date")
     
@@ -57,4 +56,49 @@ def update_log_date(request):
         return JsonResponse({'message': 'Log already exists for today', 'new_date': today.strftime('%Y-%m-%d')})
 
     logger.debug("No log found for today or yesterday. Setting date to yesterday.")
-    return JsonResponse({'message': 'No log found for today or yesterday, setting date to yesterday', 'new_date': yesterday.strftime('%Y-%m-%d')})
+    return JsonResponse({'message': 'No log found for today or yesterday, setting date to yesterday', 'new_date': yesterday.strftime('%Y-%m-%d')})"""
+    
+    
+
+def update_log_date(request):
+    logger.debug("Request received for update_log_date")
+
+    if not (correo := request.GET.get('correo')):
+        logger.error("Correo is required")
+        return JsonResponse({'error': 'Correo is required'}, status=400)
+
+    logger.debug(f"Correo received: {correo}")
+
+    now = timezone.now()
+    today = now.date()
+    yesterday = today - timedelta(days=1)
+
+    logger.debug(f"Searching for logs with correo: {correo}")
+    logs = Log.objects.filter(correo=correo, tipo_evento='SUCCESS')
+    logger.debug(f"Logs found: {logs.count()}")
+
+    if not logs.exists():
+        logger.error(f"No logs found for correo: {correo}")
+        return JsonResponse({'error': 'No logs found for this correo'}, status=404)
+
+    log_yesterday = logs.filter(fecha__date=yesterday).first()
+    if log_today := logs.filter(fecha__date=today).first():
+        # Si ya hay un registro para hoy, usamos la fecha de hoy
+        logger.debug(f"Log already exists for today with log id: {log_today.id}")
+        return JsonResponse({'message': 'Log already exists for today', 'new_date': today.strftime('%Y-%m-%d'), 'requires_justification': False})
+    elif log_yesterday:
+        # Si hay un registro para ayer, verificamos la hora actual
+        if now.hour < 12:
+            # Antes de las 12:00 pm, usamos la fecha de ayer
+            logger.debug(f"Log found for yesterday. Using yesterday's date for log id: {log_yesterday.id}")
+            return JsonResponse({'message': 'Log date is yesterday', 'new_date': yesterday.strftime('%Y-%m-%d'), 'requires_justification': False})
+        else:
+            # Después de las 12:00 pm, usamos la fecha de hoy pero requerimos justificación
+            logger.debug(
+                "Log found for yesterday but it is after 12:00 pm. Using today's date and requiring justification."
+            )
+            return JsonResponse({'message': 'Log date is today, justification required', 'new_date': today.strftime('%Y-%m-%d'), 'requires_justification': True})
+
+    # No hay registros para hoy ni para ayer, usamos la fecha de hoy sin justificación
+    logger.debug("No log found for today or yesterday. Using today's date without requiring justification.")
+    return JsonResponse({'message': 'Log date is today, no justification required', 'new_date': today.strftime('%Y-%m-%d'), 'requires_justification': False})
