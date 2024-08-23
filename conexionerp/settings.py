21e9967c326b3bff44ba9eb6346327bf8bb22b35
AngieCard <83438467+AngieCard@users.noreply.github.com>
2024-08-23 14:39:24 -0500

    cambio load static

--- conexionerp/settings.py
+++ conexionerp/settings.py
@@ -52,13 +52,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Debe ser el primero en la lista
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Debe ser el primero en la lista
+    
     
 ]
 CORS_ALLOWED_ORIGINS = [
