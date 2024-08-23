94d88153ad8df1d345b1ba851a69490b93bfdbbb
AngieCard <83438467+AngieCard@users.noreply.github.com>
2024-08-23 10:24:23 -0500

    cambio base dir

--- conexionerp/settings.py
+++ conexionerp/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 import os
 from dotenv import load_dotenv
 
@@ -18,8 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
