352c4949d564352f3b9d11fc729bdadd656b186c
Alejandro López Pérez <alejo-197@outlook.com>
2024-11-07 10:42:29 -0500

    Settings CORS updated

--- conexionerp/settings.py
+++ conexionerp/settings.py
@@ -71,7 +71,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_HEADERS = [
     "content-type",
 ]
-CSRF_TRUSTED_ORIGINS = ['https://app-conexionerp-prod-001.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://app-conexionerp-prod-001.azurewebsites.net','https://erp-apps.fundacionudea.net']
 
 CORS_ALLOW_METHODS = [
     "GET",
