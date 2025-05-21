36fa297822a5ffd77fa9358837208f347353fc51
alejo117lp <alejo-197@outlook.com>
2025-05-21 12:08:52 -0500

    Delete Prints

--- conexionerp/settings.py
+++ conexionerp/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
