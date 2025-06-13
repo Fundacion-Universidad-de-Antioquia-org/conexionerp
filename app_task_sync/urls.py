c6f979c9609f62d267300fcd3209866ab41571f7
AngieCard <83438467+AngieCard@users.noreply.github.com>
2025-06-13 15:32:32 -0500

    cambios de swagger

--- app_task_sync/urls.py
+++ app_task_sync/urls.py
@@ -1,18 +1,8 @@
 from django.urls import path,re_path
 from . import views
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="API Task Sync",
-      default_version='v1',
-      description="Documentación Swagger de la API de integración Odoo para app_task_sync",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('empleados/', views.empleados_list, name='empleados_list'),
@@ -23,6 +13,4 @@ urlpatterns = [
     path('salarios/', views.salarios_list, name='salarios_list'),
     path('estudios/', views.estudios_list, name='estudios_list'),
     path('actualizar_empleado/', views.actualizar_empleado, name='actualizar_empleado'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
